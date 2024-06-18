047febeda9c6769881ed528bf90efc5915796985
Brand Frank <3069584010@qq.com>
2024-06-18 16:11:28 +0800

    add help(vimdoc) and xml/yaml support for nvim-treesitter.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 require'nvim-treesitter.configs'.setup {
   -- 添加不同语言
   --ensure_installed = { "vim", "help", "bash", "c", "cpp", "javascript", "json", "lua", "python", "typescript", "tsx", "css", "rust", "markdown", "markdown_inline" }, -- one of "all" or a list of languages
-  ensure_installed = { "asm", "bash", "c", "c_sharp", "cmake", "cpp", "css", "cpp", "html", "java", "javascript", "json", "kotlin", "lua", "make", "markdown", "markdown_inline", "python", "ruby", "rust", "tsx", "typescript", "vim" }, -- one of "all" or a list of languages
+  ensure_installed = { "asm", "bash", "c", "c_sharp", "cmake", "cpp", "css", "cpp", "html", "java", "javascript", "json", "kotlin", "lua", "make", "markdown", "markdown_inline", "python", "ruby", "rust", "tsx", "typescript", "vim", "vimdoc", "xml", "yaml" }, -- one of "all" or a list of languages
 
   highlight = { enable = true },
   indent = { enable = true },
