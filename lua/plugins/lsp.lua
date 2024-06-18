65acb13f4a115d18b54931c659952fcb8de225ab
Brand Frank <3069584010@qq.com>
2024-06-18 14:13:26 +0800

    Add pylyzer and cmake lsp-server. Set up clangd/pylyzer/cmake.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -12,12 +12,26 @@ require("mason").setup({
 require("mason-lspconfig").setup({
   -- 确保安装，根据需要填写
   ensure_installed = {
-    "lua_ls", "clangd"
+    "lua_ls", "clangd", "pylyzer", "cmake"
   },
 })
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
+-- 设置mason lspconfig后，您可以通过lspcnfig设置服务器
+-- lua_ls
 require("lspconfig").lua_ls.setup {
+  capabilities = capabilities,
+}
+-- clangd
+require("lspconfig").clangd.setup {
+  capabilities = capabilities,
+}
+-- pylyzer
+require("lspconfig").pylyzer.setup {
+  capabilities = capabilities,
+}
+-- cmake-language-server
+require("lspconfig").cmake.setup {
   capabilities = capabilities,
 }
