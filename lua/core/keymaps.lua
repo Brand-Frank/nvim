f5dee8dff8ef044c75b2e8963661bd29d1a0d353
Brand Frank <3069584010@qq.com>
2024-06-18 12:14:03 +0800

    Add keymaps for windows-close and tab-exchange.

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -14,6 +14,8 @@ keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 -- 窗口
 keymap.set("n", "<leader>sv", "<C-w>v") -- 水平新增窗口 
 keymap.set("n", "<leader>sh", "<C-w>s") -- 垂直新增窗口
+-- <C-w>c	关闭当前窗口。
+-- <C-w>o	仅保留当前窗口，关闭其他窗口，效果等同于:only命令（关闭除了当前窗口之外的所有其他窗口）
 
 -- 取消高亮
 keymap.set("n", "<leader>nh", ":nohl<CR>")
@@ -21,6 +23,9 @@ keymap.set("n", "<leader>nh", ":nohl<CR>")
 -- 切换buffer
 keymap.set("n", "<C-L>", ":bnext<CR>")
 keymap.set("n", "<C-H>", ":bprevious<CR>")
+
+keymap.set("n", "<C-Left>", ":tabpre<CR>")	-- Ctrl+左方向键		切换到上一个标签页
+keymap.set("n", "<C-Right>", ":tabnext<CR>")	-- Ctrl+右方向键		切换到下一个标签页
 
 -- ---------- 插件 ---------- ---
 -- nvim-tree
